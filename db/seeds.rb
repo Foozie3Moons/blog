c44f37fd325dc076848e8dbf6c4081d4dd96b0e5
foozie3moons <matthew.bell.290@gmail.com>
2018-02-05 12:13:03 -0800

    creating random fake data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.destroy_all
+Post.destroy_all
+
+(1..20).each do |i|
+  User.create!(email: "matthew.bell.#{i}@email.com", password: "password", name: "Matt ##{i}")
+end
+(1..100).each do |i|
+  users = User.all
+  Post.create!(title: "Lipsum Post #{i}", body: %{
+      Nullam hendrerit iaculis sodales. Curabitur varius nibh arcu, id molestie nibh fermentum vitae. Cras quis semper dui. Cras porttitor urna sit amet risus vehicula tempor. Maecenas quis tempor ligula. Donec et nibh eu leo volutpat placerat. Fusce vulputate elit in nisi pretium, vel fermentum mi fermentum. Mauris scelerisque, lectus non luctus ultricies, urna eros tincidunt risus, at varius sapien diam id erat.
+  }, user: users[rand(20).ceil])
+end
