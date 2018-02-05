fa145bf1b7f68f55d8b98c902924ae9390988721
foozie3moons <matthew.bell.290@gmail.com>
2018-02-05 11:03:06 -0800

    updated model relations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,11 @@
 class Post
   include Mongoid::Document
-  field :title, type: Sting
+  include Mongoid::Timestamps
+  field :title, type: String
   field :body, type: String
-  embedded_in :author
+  belongs_to :user
+
+  validates_presence_of :title, :user, :body
+  validates_uniqueness_of :title
+
 end
