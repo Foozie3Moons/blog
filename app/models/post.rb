ae15fd16938775660cbca1153c02f5b44604d242
foozie3moons <matthew.bell.290@gmail.com>
2018-02-05 12:13:31 -0800

    updated layout a bit

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,11 @@
 class Post
   include Mongoid::Document
   include Mongoid::Timestamps
+
   field :title, type: String
   field :body, type: String
   belongs_to :user
 
   validates_presence_of :title, :user, :body
   validates_uniqueness_of :title
-
 end
